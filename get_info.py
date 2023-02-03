98b7ad9477c3e4c925360b6327ab86a5e5663e82
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-02-03 13:23:51 -0500

    yahoo api function improved

--- get_info.py
+++ get_info.py
@@ -1,6 +1,6 @@
 import requests
 import yfinance as yf
-
+import pprint
 def main(ticker):
     stock = yf.Ticker(ticker)
     print(stock.basic_info)
@@ -21,8 +21,34 @@ def main(ticker):
     print(stock.get_income_stmt())
 
 
+def get_general_yahoo_info(ticker:str)-> dict:
+    stock = yf.Ticker(ticker)
+    #infos = stock.fast_info
+    #pprint.pprint(infos)
+    infos = stock.info
+    print(infos)
+    return infos
 
+def get_financial_yahoo_info(ticker:str)-> dict:
+    
+    stock = yf.Ticker(ticker)
 
+    #finantials
+    print("## Income Statement ##")
+    income_stmt = stock.income_stmt
+    quarterly_income_stmt = stock.quarterly_income_stmt
+
+    print("## Balance Sheet ##")
+    balance_sheet = stock.balance_sheet
+    quarterly_balance_sheet = stock.quarterly_balance_sheet
+
+    print("## Cash Flow ##")
+    cashflow = stock.cashflow
+    quarterly_cashflow = stock.quarterly_cashflow
+
+    print(stock.get_income_stmt())
+    return income_stmt, quarterly_income_stmt, balance_sheet, quarterly_balance_sheet, cashflow, quarterly_cashflow
 
 if __name__ == '__main__':
-    main('TSLA')
+    #main('TSLA')
+    get_general_info('TSLA')
