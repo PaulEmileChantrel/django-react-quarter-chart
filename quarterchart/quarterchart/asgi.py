a67f20f0447f3615e3f0c8d428b6960ad7e452e9
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-02-03 18:42:05 -0500

    django set up

---
+++ quarterchart/quarterchart/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for quarterchart project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quarterchart.settings')
+
+application = get_asgi_application()
