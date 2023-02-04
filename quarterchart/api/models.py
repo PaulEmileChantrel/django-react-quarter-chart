e868e4cbefc0f0ffeba748f054f44cde46c55a77
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-02-03 21:16:01 -0500

    starting frontend with react

--- quarterchart/api/models.py
+++ quarterchart/api/models.py
@@ -6,3 +6,5 @@ class Compagnies(models.Model):
     ticker = models.CharField(max_length=6)
     data_was_downloaded = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    image_link = models.CharField(max_length=250)
+    market_cap = models.FloatField(default=0)
