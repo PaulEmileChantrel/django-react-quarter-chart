bee68bb97e585a64714cfc3401627422795aa519
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-02-03 21:45:16 -0500

    react functional

---
+++ quarterchart/frontend/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <div>Testing React Code</div>;
+        
+    }
+
+
+}
+
+const appDiv = document.getElementById('app');
+render(<App />, appDiv);
