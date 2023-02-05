a9b64307e8a4f6ccb84db3e541ebdf95414d96d4
PaulEmileChantrel <paul-emile.chantrel.1@ulaval.ca>
2023-02-05 18:32:53 -0500

    Starting main page

--- quarterchart/frontend/src/components/HomePage.js
+++ quarterchart/frontend/src/components/HomePage.js
@@ -1,14 +1,43 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import  Grid  from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import TextField  from '@material-ui/core/TextField';  
+import FormHelperText from '@material-ui/core/FormHelperText';
+import  FormControl from '@material-ui/core/FormControl';
+import {Link} from 'react-router-dom';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-import ChartPage from './ChartPage';
-import CompanieInfoPage from './CompanieInfoPage';
 
 
 export default function HomePage () {
     
 
   
-    return (<p> This is the home page</p>)
+    return (
+        <Grid container spacing={1}>
+
+            <Grid item xs={12} align="center">
+                <Typography component="h5" variant="h5" > Companies List</Typography>
+            </Grid>
+            
+            <Grid item xs={12} align="center">
+                <FormControl component="fieldset">
+                    <TextField required={true} type="text" />
+                    <FormHelperText >
+                        <div align="center">Filter companies by name</div>
+                    </FormHelperText>
+                </FormControl>
+                <Button variant="contained" color="primary">Search</Button>
+            </Grid>
+            <Grid item xs={12} align="center">
+                
+            </Grid>
+        </Grid>
+        
+    )
 
 };
 
