e23fbe509679b8427b0f0df910a32ca346af32b9
Paul-Emile Chantrel <paul-emile.chantrel.1@ulaval.ca>
2023-02-15 20:36:26 -0500

    improvement of company table

--- quarterchart/frontend/src/components/CompaniesList.js
+++ quarterchart/frontend/src/components/CompaniesList.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Company from './Company';
+import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
 
 
 export default function CompaniesList({companies_list}){
     
     return (
-        companies_list.map(company =>{
-            return <Company key={company.id}  company={company}/>
-        })
+        <MDBTable align='middle'>
+        <MDBTableHead>
+            <tr>
+            <th scope='col'>Name</th>
+            <th scope='col'>Market Cap.</th>
+            
+            </tr>
+        </MDBTableHead>
+        <MDBTableBody>
+            {companies_list.map(company =>{
+                return <Company key={company.id}  company={company}/>
+            })}
+        </MDBTableBody>
+        </MDBTable>
       )
 }
--- quarterchart/frontend/src/components/Company.js
+++ quarterchart/frontend/src/components/Company.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import formatMktCp from './formatMktCap';
+import { MDBBadge, MDBBtn, MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
+
 export default function Company({company}) {
     
     return (
-        <div>
-            <a href={'/chart/' + company.ticker}>
-
-                <p>
-                    {company.name} ({company.ticker}) | Market Cap: {formatMktCp(company.market_cap)}
-                </p>
-            </a>
+        <tr>
+          <td>
+            <div className='d-flex align-items-center' href={'/chart/' + company.ticker} >
+              <img
+                src={'/static/images/company_logo/'+company.ticker.lower()+'.webp'}
+                alt=''
+                style={{ width: '45px', height: '45px' }}
+                className='rounded-circle'
+              />
+              <div className='ms-3'>
+                <p className='fw-bold mb-1'>{company.name}</p>
+                <p className='text-muted mb-0'>{company.ticker}</p>
+              </div>
+            </div>
+          </td>
+          <td>
+            <p className='fw-normal mb-1'>{formatMktCp(company.market_cap)}</p>
             
-            
-        </div>
+          </td>
+          
+        </tr>
+        
+        
     )
 }
